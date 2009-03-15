26089c68ac720a739a53f9ff2bd2836a29a0d965
will <will@spanner.org>
2009-03-15 11:52:12 +0000

    now independent of multi_site

--- lib/fake_site_scope.rb
+++ lib/fake_site_scope.rb
@@ -1,9 +1,10 @@
 module FakeSiteScope
 
   def self.included(base)
-    base.class_eval do
+    class << base
       unless methods.include?('is_site_scoped') 
         define_method("is_site_scoped") { |*args| STDERR.puts "Multi_site not installed or not correct version: #{self} is not site-scoped." }
+        define_method("is_site_scoped?") { |*args| false }
       end
     end
   end  
