96bc4d66d2288db8088d4571f450dc913c132718
spanner <will@spanner.org>
2010-09-27 11:20:26 +0100

    controller whitelist updated

--- app/controllers/admin/reader_settings_controller.rb
+++ app/controllers/admin/reader_settings_controller.rb
@@ -7,7 +7,7 @@ class Admin::ReaderSettingsController < ApplicationController
   before_filter :get_setting, :only => [:show, :edit, :update]
   cattr_accessor :settable
   # this will need to be extensible
-  @@settable = ['reader.allow_registration?', 'reader.require_confirmation?', 'reader.layout', 'site.title', 'site.url', 'reader.email_from_name', 'reader.email_from_address']
+  @@settable = ['reader.allow_registration?', 'reader.require_confirmation?', 'reader.layout', 'site.title', 'site.url', 'reader.mail_from_name', 'reader.mail_from_address']
 
   def self.make_settable(*keys)
     @@settable += keys
