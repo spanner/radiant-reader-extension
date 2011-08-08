981af662b04ab965b1d91e7ac045d724c8d4ac12
spanner <will@spanner.org>
2011-08-08 15:08:24 +0100

    new group fix. closes #45. thanks

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -1,4 +1,6 @@
 class Admin::GroupsController < Admin::ResourceController
+  helper :reader
+  paginate_models
   skip_before_filter :load_model
   before_filter :load_model, :except => :index    # we want the filter to run before :show too
   
