89f02a01a31868040bb7f4c0c92c734f5cd2ced4
William Ross <will@spanner.org>
2011-09-03 12:03:27 +0100

    simple unobtrusive group management js with (for compatibility reasons) lowpro

--- public/javascripts/admin/reader.js
+++ public/javascripts/admin/reader.js
@@ -1,3 +1,5 @@
+// This is your 'select all' checkbox: its state is applied to all siblings with 'toggled' class.
+//
 Toggle.SelectAllBehavior = Behavior.create(Toggle.CheckboxBehavior, {
   toggle: function() {
     var state = this.element.checked;
@@ -5,7 +7,26 @@ Toggle.SelectAllBehavior = Behavior.create(Toggle.CheckboxBehavior, {
   }
 });
 
+// This is a normal remote link that replaces itself with the response.
+//
+Remote.UpdatingLink = Behavior.create(Remote.Base, {
+  onclick : function() {
+    var self = this;
+    var options = Object.extend({ 
+      url : this.element.href, 
+      method : 'get',
+      update: this.element.up(),
+      onLoading: function () { self.element.addClassName('waiting'); },
+      onComplete: function () { self.element.removeClassName('waiting'); },
+      onSuccess: function () { Event.addBehavior.reload(); },
+      onFailure: function () { self.element.addClassName('failed'); }
+    }, self.options);
+    return self._makeRequest(options);
+  }
+});
+
 Event.addBehavior({ 
   'div.radio_group': Toggle.RadioGroupBehavior(),
-  'input.select_all': Toggle.SelectAllBehavior()
+  'input.select_all': Toggle.SelectAllBehavior(),
+  'a.fake_checkbox': Remote.UpdatingLink()
 });
