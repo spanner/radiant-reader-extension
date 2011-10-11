2defe550aede1919045b63f3898a920abbfb5d7e
William Ross <will@spanner.org>
2011-10-11 10:05:01 +0100

    locale string mixup

--- lib/site_controller_extensions.rb
+++ lib/site_controller_extensions.rb
@@ -8,9 +8,9 @@ module SiteControllerExtensions
       def find_page_with_group_check(url)
         page = find_page_without_group_check(url)
         if page
-          raise ReaderError::LoginRequired, t("reader_extension.page_is_private_please_log_in") if page.restricted? && !current_reader
-          raise ReaderError::ActivationRequired, t("reader_extension.page_is_private_please_log_in") if page.restricted? && !current_reader.activated?
-          raise ReaderError::AccessDenied, t("reader_extension.page_access_not_given")  unless page.visible_to?(current_reader)
+          raise ReaderError::LoginRequired, t("reader_extension.page_not_public") if page.restricted? && !current_reader
+          raise ReaderError::ActivationRequired, t("reader_extension.page_not_public") if page.restricted? && !current_reader.activated?
+          raise ReaderError::AccessDenied, t("reader_extension.page_permission_denied")  unless page.visible_to?(current_reader)
         end
         page
       end
